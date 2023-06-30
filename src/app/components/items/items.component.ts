34d6ab61203d726f846abdec1b5d19a07fc8ee82
danaaguilar <danaaguilar2017@gmail.com>
2023-06-30 16:52:48 -0300

    cree los 3 componentes y la carpeta models. en header agregue un navbar, en items hice en ngFor y los distintos items, en item hice muestro los items y en models se encuentran los datos de cada item

---
+++ src/app/components/items/items.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/app/models/item';
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css']
+})
+export class ItemsComponent implements OnInit{
+
+  items: Item[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        id: 0,
+        title: "manzana",
+        price: 10.5,
+        quantity: 4,
+        completed: false
+      },
+      {
+        id: 1,
+        title: "banana",
+        price: 8.5,
+        quantity: 5,
+        completed: true
+      },
+      {
+        id: 2,
+        title: "frutilla",
+        price: 17.5,
+        quantity: 10,
+        completed: false
+      }
+    ]
+  }
+
+}
